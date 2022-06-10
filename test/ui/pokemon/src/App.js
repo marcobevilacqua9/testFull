b974f79bbf8add80e9f5bbf1fad63b7ff82dd9d5
marcobevilacqua9 <101326406+marcobevilacqua9@users.noreply.github.com>
2022-06-10 14:37:20 +0200

    Add files via upload

---
+++ test/ui/pokemon/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PokemonScreen from './pokemonScreen';
+
+function App() {
+
+  return (
+    <div className="App">
+       <PokemonScreen />
+    </div>
+  );
+}
+
+export default App;
